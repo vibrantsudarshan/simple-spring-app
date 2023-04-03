912559cc987d85d94f8dc8a1087743dae7cf2639
vibrantsudarshan <105799949+vibrantsudarshan@users.noreply.github.com>
2023-04-03 16:46:01 -0400

    Updated display message

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -8,6 +8,6 @@ public class HomeController {
     
     @GetMapping("/")
     public String home() {
-        return "Hello from Azure App Service (in the staging slot)!";
+        return "Hello from Sudarshan's V2 Azure App Service (in the production slot)!";
     }
 }
